e3f3206e555f84578d3d30dd547b55098fc23e8d
Maria Bilous <maria.bilous101@gmail.com>
2019-12-02 17:25:15 +0200

    HW_4: Note entity class added to Model

--- src/main/java/HW_4_notebook/version_1/Controller.java
+++ src/main/java/HW_4_notebook/version_1/Controller.java
@@ -4,13 +4,13 @@ package HW_4_notebook.version_1;
  * Created by Bilous M. 29.11.2019
  */
 public class Controller {
-//    private Model model;
-//    private View view;
+    private Model model;
+    private View view;
 
-//    public Controller(Model model, View view) {
-//        this.model = model;
-//        this.view = view;
-//    }
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
 
     //The Work method
     public void processUser() {
