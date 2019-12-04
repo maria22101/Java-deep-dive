f12b75aa3f7e00560a2b22ecc1022fed937487d1
Maria Bilous <maria.bilous101@gmail.com>
2019-12-04 16:16:28 +0200

    TASK_1: Utility method depositsSortedByRate() implemented, the test passed

--- src/main/java/task_1/model/Utility.java
+++ src/main/java/task_1/model/Utility.java
@@ -3,7 +3,9 @@ package task_1.model;
 import task_1.model.entity.Deposit;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utility {
     private List<Deposit> deposits = new ArrayList<Deposit>();
@@ -15,7 +17,18 @@ public class Utility {
     }
 
     public List<Deposit> depositsSortedByRate() {
-        return new ArrayList<Deposit>(); // to implement after test
+        return deposits
+                .stream()
+                .sorted(new Comparator<Deposit>() {
+                    @Override
+                    public int compare(Deposit o1, Deposit o2) {
+                        if (o1.getInterestRate() < o2.getInterestRate()) {
+                            return -1;
+                        }else {
+                            return 1;
+                        }
+                    }})
+                .collect(Collectors.toList());
     }
 
     public List<Deposit> depositsSortedByBanks() {
