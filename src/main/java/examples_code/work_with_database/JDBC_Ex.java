1f5b8f012a7dbd8017dcf47bc83a60573e074bcf
Maria Bilous <maria.bilous101@gmail.com>
2019-12-29 22:04:58 +0200

    JDBC: some sql requests using statement, try-with-resources example

--- src/main/java/examples_code/work_with_database/JDBC_Ex.java
+++ src/main/java/examples_code/work_with_database/JDBC_Ex.java
@@ -1,9 +1,6 @@
 package examples_code.work_with_database;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBC_Ex {
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
@@ -11,25 +8,23 @@ public class JDBC_Ex {
     private static final String PASSWORD = "****";
 
     public static void main(String[] args) {
-        Connection connection;
-        Driver driver;
 
         try {
-            driver = new com.mysql.cj.jdbc.Driver();
+            Driver driver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
-            if(!connection.isClosed()) {
-                System.out.println("Connection to the databese is set up.");
-            }
-
-            connection.close();
-
-            if(connection.isClosed()) {
-                System.out.println("Connection closed.");
-            }
         } catch (SQLException e) {
             System.out.println("Driver not registered");
+        }
+
+        // try-with-resources example
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                Statement statement = connection.createStatement()) {
+
+            statement.execute("DELETE FROM users WHERE id=7");
+            statement.executeUpdate("UPDATE users SET name='Ron' WHERE id=5");
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 }
